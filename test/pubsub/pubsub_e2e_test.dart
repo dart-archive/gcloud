afea461221bc09fae62e641abe128e29b39326e0
Kevin Moore <kevmoo@google.com>
2019-01-16 09:03:10 -0800

    Enable non-e2e tests on Travis

--- test/pubsub/pubsub_e2e_test.dart
+++ test/pubsub/pubsub_e2e_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@Tags(const ["e2e"])
+
 import 'package:gcloud/pubsub.dart';
 import 'package:http/http.dart';
 import 'package:test/test.dart';
