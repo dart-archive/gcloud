7ac7489d0b3f80c666cbaf2883b3aeca0bf01d91
Matias Meno <m@tias.me>
2019-09-25 15:26:21 +0200

    Refactor _isExpandoClass to use ClassMirror.isSubtypeOf

--- lib/src/db/model_db_impl.dart
+++ lib/src/db/model_db_impl.dart
@@ -352,17 +352,8 @@ class ModelDBImpl implements ModelDB {
     return properties;
   }
 
-  bool _isExpandoClass(mirrors.ClassMirror modelClass) {
-    while (modelClass.superclass != modelClass) {
-      if (modelClass.reflectedType == ExpandoModel) {
-        return true;
-      } else if (modelClass.reflectedType == Model) {
-        return false;
-      }
-      modelClass = modelClass.superclass;
-    }
-    throw StateError('This should be unreachable.');
-  }
+  bool _isExpandoClass(mirrors.ClassMirror modelClass) =>
+      modelClass.isSubtypeOf(mirrors.reflectClass(ExpandoModel));
 }
 
 class _ModelDescription<T extends Model> {
