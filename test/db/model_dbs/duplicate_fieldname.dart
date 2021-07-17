bfd93be813d23f23306a1b3b8cafc0ec98ad81dd
Jonas Finnemann Jensen <jonasfj@google.com>
2021-07-17 20:55:54 +0200

    Fix formatting

--- test/db/model_dbs/duplicate_fieldname.dart
+++ test/db/model_dbs/duplicate_fieldname.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-
 library gcloud.db.model_test.duplicate_fieldname;
 
 import 'package:gcloud/db.dart' as db;
